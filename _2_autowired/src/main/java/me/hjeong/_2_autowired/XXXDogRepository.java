f1b830915c29e47c69bdcf66db890f92942f838f
효정 <hjeong.you@gmail.com>
2020-05-12 14:47:09 +0900

    2 @Autowired: @Primary

--- _2_autowired/src/main/java/me/hjeong/_2_autowired/XXXDogRepository.java
+++ _2_autowired/src/main/java/me/hjeong/_2_autowired/XXXDogRepository.java
@@ -1,7 +1,11 @@
 package me.hjeong._2_autowired;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@Primary
 public class XXXDogRepository implements DogRepository {
 }
+// DogRepository 타입으로 빈을 주입받을 때
+// @Primary가 붙은 타입이 우선순위가 제일 높은 후보가 된다.
