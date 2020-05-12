f24e79f7f4ba4d0dd25c238a0274ecdc1aaa2d62
효정 <hjeong.you@gmail.com>
2020-05-12 13:49:41 +0900

    1 스프링 IoC 컨테이너와 빈: 프로젝트 세팅: component-scan(xml)

--- _1_ioc_container_and_bean/src/main/java/me/hjeong/_1_ioc_container_and_bean/Application.java
+++ _1_ioc_container_and_bean/src/main/java/me/hjeong/_1_ioc_container_and_bean/Application.java
@@ -6,8 +6,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import java.util.Arrays;
 
 public class Application {
-
-    public static void main(String[] args) {
+    // [1] 직접 빈으로 등록하기
+    public static void main1(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
 
         String[] beanDefinitionNames = context.getBeanDefinitionNames();
@@ -25,4 +25,19 @@ public class Application {
     // bookRepository 객체를 따로 생성하지 않아도
     // 스프링 IoC 컨테이너가 객체를 생성하고(그 객체를 빈이라고 한다) 컨테이너 안에 가지고 있다가
     // 다른 빈(bookService)에서 빈(bookRepository)으로 주입받아 사용할 수 있다.
+
+    /******************************************************************************************/
+
+    // [2] xml 빈 설정 파일에서 component-scan 을 이용하기
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("application2.xml");
+
+        String[] beanDefinitionNames = context.getBeanDefinitionNames();
+        System.out.println(Arrays.toString(beanDefinitionNames));
+
+        BookService bookService = (BookService) context.getBean("bookService");
+        System.out.println(bookService.bookRepository != null);
+    }
+
+
 }
--- _1_ioc_container_and_bean/src/main/java/me/hjeong/_1_ioc_container_and_bean/BookRepository.java
+++ _1_ioc_container_and_bean/src/main/java/me/hjeong/_1_ioc_container_and_bean/BookRepository.java
@@ -1,4 +1,7 @@
 package me.hjeong._1_ioc_container_and_bean;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class BookRepository {
 }
