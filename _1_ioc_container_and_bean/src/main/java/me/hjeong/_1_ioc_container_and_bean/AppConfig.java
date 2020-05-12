0acb53cb65cbca5eae3347a1956fedf3ef1eae0b
효정 <hjeong.you@gmail.com>
2020-05-12 14:11:23 +0900

    1 스프링 IoC 컨테이너와 빈: java 빈 설정 파일에서 의존성 주입하기(component scan)

--- _1_ioc_container_and_bean/src/main/java/me/hjeong/_1_ioc_container_and_bean/AppConfig.java
+++ _1_ioc_container_and_bean/src/main/java/me/hjeong/_1_ioc_container_and_bean/AppConfig.java
@@ -1,6 +1,7 @@
 package me.hjeong._1_ioc_container_and_bean;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration // 빈 설정 파일. 이 안에서 빈 명세를 작성한다.
@@ -36,3 +37,19 @@ public class AppConfig {
 
 // @Configuration 을 붙이면 기본적인 빈들과 우리가 만든 함수명을 빈 이름으로 한 빈이 만들어진다.
 // @Bean 을 이용해서 직접 빈을 만들 때는 setter, argument 로 의존성을 주입할 수 있다.
+
+@Configuration
+@ComponentScan(basePackages = "me.hjeong._1_ioc_container_and_bean")
+class AppConfig2 {
+    @Bean
+    public BookRepository bookRepository() {
+        return new BookRepository();
+    }
+    @Bean
+    public BookService bookService() {
+        return new BookService();
+    }
+}
+// setter나 arguemnt로 의존성을 주입하는 것이 귀찮다면
+// @ComponentScan + 의존성이 필요한 부분에서 @Autowired 써주기
+// 로 편리하게 의존성을 주입시킬 수 있다.
