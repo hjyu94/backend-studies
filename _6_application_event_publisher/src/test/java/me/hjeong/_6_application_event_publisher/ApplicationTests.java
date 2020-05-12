ec1b825764e4312b0b299be757feb8e5b3cceef9
효정 <hjeong.you@gmail.com>
2020-05-12 18:09:27 +0900

    6 ApplicationEventPublisher: 프로젝트 세팅

---
+++ _6_application_event_publisher/src/test/java/me/hjeong/_6_application_event_publisher/ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hjeong._6_application_event_publisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
