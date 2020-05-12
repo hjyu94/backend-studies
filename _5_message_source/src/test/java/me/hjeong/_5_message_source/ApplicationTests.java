bc8cf0158de057045de2164e724d87db42aee77e
효정 <hjeong.you@gmail.com>
2020-05-12 17:01:26 +0900

    5 MessageSource: 프로젝트 세팅

---
+++ _5_message_source/src/test/java/me/hjeong/_5_message_source/ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hjeong._5_message_source;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
