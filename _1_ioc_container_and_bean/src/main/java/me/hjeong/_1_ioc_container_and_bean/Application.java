9a6e7618654090506f3bf91a84df2d26b66351de
효정 <hjeong.you@gmail.com>
2020-05-12 13:21:47 +0900

    1 스프링 IoC 컨테이너와 빈: 프로젝트 세팅

---
+++ _1_ioc_container_and_bean/src/main/java/me/hjeong/_1_ioc_container_and_bean/Application.java
@@ -0,0 +1,13 @@
+package me.hjeong._1_ioc_container_and_bean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
