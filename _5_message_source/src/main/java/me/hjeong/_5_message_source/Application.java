f22c57a44d886011f48e4f97a208b4713990947e
효정 <hjeong.you@gmail.com>
2020-05-12 17:43:51 +0900

    5 MessageSource: 사용하는 언어별로 프로퍼티에서 메세지 꺼내기

--- _5_message_source/src/main/java/me/hjeong/_5_message_source/Application.java
+++ _5_message_source/src/main/java/me/hjeong/_5_message_source/Application.java
@@ -2,6 +2,9 @@ package me.hjeong._5_message_source;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +13,14 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setCacheSeconds(3);
+        return messageSource;
+    }
+    // 릴로드 기능?
+    // 앱 구동 후 프로퍼티 값을 변경하고 빌드하면 앱을 재가동하지 않아도 자동으로 변경된 프로퍼티 값이 찍힌다.
 }
