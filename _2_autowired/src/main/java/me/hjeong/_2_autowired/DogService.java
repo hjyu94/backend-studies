e872c9fa8aea424e83ad75ed64d98ae761af743d
효정 <hjeong.you@gmail.com>
2020-05-12 14:48:19 +0900

    2 @Autowired: @Qualifier

--- _2_autowired/src/main/java/me/hjeong/_2_autowired/DogService.java
+++ _2_autowired/src/main/java/me/hjeong/_2_autowired/DogService.java
@@ -1,11 +1,13 @@
 package me.hjeong._2_autowired;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DogService {
     @Autowired
+    @Qualifier("XXXDogRepository")
     DogRepository dogRepository;
     // DogRepository interface를 MyDogRepository, AnotherDogRepository 에서 구현하고 있다.
     // 이 경우 어떤 DogRepository 를 써야 할 지 모른다.
