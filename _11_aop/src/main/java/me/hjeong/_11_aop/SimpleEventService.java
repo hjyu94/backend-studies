ecd3237984d3be272fa638c82966688d4f190ddd
효정 <hjeong.you@gmail.com>
2020-05-13 16:10:41 +0900

    11 AOP: 메소드의 실행 시간을 재자 (real subject에 직접 구현)

--- _11_aop/src/main/java/me/hjeong/_11_aop/SimpleEventService.java
+++ _11_aop/src/main/java/me/hjeong/_11_aop/SimpleEventService.java
@@ -7,14 +7,18 @@ import org.springframework.stereotype.Component;
 public class SimpleEventService implements EventService {
     @Override
     public void createEvent() throws InterruptedException {
+        long begin = System.currentTimeMillis();
         Thread.sleep(1000l);
         System.out.println("create event");
+        System.out.println(System.currentTimeMillis() - begin + "ms passed");
     }
 
     @Override
     public void publishEvent() throws InterruptedException {
+        long begin = System.currentTimeMillis();
         Thread.sleep(3000l);
         System.out.println("publish event");
+        System.out.println(System.currentTimeMillis() - begin + "ms passed");
     }
 
     @Override
