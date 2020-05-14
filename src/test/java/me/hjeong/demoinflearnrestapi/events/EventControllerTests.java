8abf83ab2aaa663047641d41b65151375363327b
효정 <hjeong.you@gmail.com>
2020-05-14 15:03:36 +0900

    POST /api/events returns 201

--- src/test/java/me/hjeong/demoinflearnrestapi/events/EventControllerTests.java
+++ src/test/java/me/hjeong/demoinflearnrestapi/events/EventControllerTests.java
@@ -1,5 +1,7 @@
+
 package me.hjeong.demoinflearnrestapi.events;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,8 +11,11 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -20,16 +25,33 @@ public class EventControllerTests {
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
     @Test
     public void createEvent() throws Exception {
+        Event event = Event.builder()
+                .name("Spring")
+                .description("REST API Development with Spring")
+                .beginEnrollmentDateTime(LocalDateTime.of(2018, 9, 1, 0, 0))
+                .closeEnrollmentDateTime(LocalDateTime.of(2018, 10, 31, 0, 0))
+                .beginEventDateTime(LocalDateTime.of(2018, 11, 23, 14, 0))
+                .endEventDateTime(LocalDateTime.of(2018,11,24,14,0))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남역 D2 스타텁 팩토리")
+                .build();
 
         mockMvc.perform(post("/api/events/")
-                    .contentType(MediaType.APPLICATION_JSON_VALUE) // 응답 요청은 json으로 생겼다.
-                    .accept(MediaTypes.HAL_JSON_VALUE) // HAL, HATEOS,
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .accept(MediaTypes.HAL_JSON_VALUE)
+                .content(objectMapper.writeValueAsString(event))
                 )
                 .andDo(print())
                 .andExpect(status().isCreated())
                 .andExpect(status().is(201))
+                .andExpect(jsonPath("id").exists())
         ;
     }
 }
