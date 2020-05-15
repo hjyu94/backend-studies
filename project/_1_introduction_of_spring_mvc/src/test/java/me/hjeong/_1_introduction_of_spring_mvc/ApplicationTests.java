a378093f0008e53d8fe1f733dbcc66ae1eead9ae
효정 <hjeong.you@gmail.com>
2020-05-15 14:22:14 +0900

    1 스프링 MVC 소개: 프로젝트 세팅

---
+++ project/_1_introduction_of_spring_mvc/src/test/java/me/hjeong/_1_introduction_of_spring_mvc/ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hjeong._1_introduction_of_spring_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
