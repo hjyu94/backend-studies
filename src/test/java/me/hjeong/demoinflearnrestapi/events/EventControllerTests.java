f1415ac9783b3d28ee77629af4da5c0e5c7154c6
효정 <hjeong.you@gmail.com>
2020-05-19 18:31:29 +0900

    GET /api 구현, 에러 본문에 /index 링크 추가
    return ResponseEntity.badRequest().body(new ErrorsResource(errors));
    class ErrorsResource extends EntityModel<Errors>
    ErrorsResource.add(linkTo(methodOn(IndexController.class).index()).withRel("index"));

--- src/test/java/me/hjeong/demoinflearnrestapi/events/EventControllerTests.java
+++ src/test/java/me/hjeong/demoinflearnrestapi/events/EventControllerTests.java
@@ -224,9 +224,10 @@ public class EventControllerTests {
                 )
                 .andDo(print())
                 .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$[0].objectName").exists())
-                .andExpect(jsonPath("$[0].defaultMessage").exists())
-                .andExpect(jsonPath("$[0].code").exists())
+                .andExpect(jsonPath("content[0].objectName").exists())
+                .andExpect(jsonPath("content[0].defaultMessage").exists())
+                .andExpect(jsonPath("content[0].code").exists())
+                .andExpect(jsonPath("_links.index").exists())
         ;
     }
 }
