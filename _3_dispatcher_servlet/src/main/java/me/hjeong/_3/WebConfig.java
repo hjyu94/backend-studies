17557b8e5467f651455ac46f0cc140351780e5fc
효정 <hjeong.you@gmail.com>
2020-05-19 19:07:29 +0900

    custom viewResolver
    - @Bean InternalResourceViewResolver
    - setPrefix(), setSuffix()

--- _3_dispatcher_servlet/src/main/java/me/hjeong/_3/WebConfig.java
+++ _3_dispatcher_servlet/src/main/java/me/hjeong/_3/WebConfig.java
@@ -1,10 +1,19 @@
 package me.hjeong._3;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.mvc.Controller;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @ComponentScan //(useDefaultFilters = false, includeFilters = @ComponentScan.Filter(Controller.class))
 public class WebConfig {
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
 }
