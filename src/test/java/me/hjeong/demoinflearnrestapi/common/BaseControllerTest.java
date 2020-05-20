acbf5176b11e53c3082ad0c5079e6c8cf26b104f
효정 <hjeong.you@gmail.com>
2020-05-20 22:00:05 +0900

    테스트 코드 리팩토링
    테스트 클래스 상속받기, @Ignore

--- src/test/java/me/hjeong/demoinflearnrestapi/common/BaseControllerTest.java
+++ src/test/java/me/hjeong/demoinflearnrestapi/common/BaseControllerTest.java
@@ -1,9 +1,9 @@
-package me.hjeong.demoinflearnrestapi.index;
+package me.hjeong.demoinflearnrestapi.common;
 
-
-import me.hjeong.demoinflearnrestapi.common.RestDocsConfiguration;
-import org.junit.Test;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Ignore;
 import org.junit.runner.RunWith;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -13,26 +13,20 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 @AutoConfigureRestDocs
 @ActiveProfiles("test")
 @Import(RestDocsConfiguration.class)
-public class IndexControllerTest {
+@Ignore
+public class BaseControllerTest {
+    @Autowired
+    protected MockMvc mockMvc;
 
     @Autowired
-    MockMvc mockMvc;
+    protected ObjectMapper objectMapper;
 
-    @Test
-    public void index() throws Exception {
-        this.mockMvc.perform(get("/api"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("_links.events").exists())
-        ;
-    }
+    @Autowired
+    protected ModelMapper modelMapper;
 }
