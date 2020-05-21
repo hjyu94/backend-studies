bfe2bf96869e54ef9430e27d61a075b9db593fcd
효정 <hjeong.you@gmail.com>
2020-05-21 18:02:19 +0900

    스프링 시큐리티 기본 설정
    1. AppConfig
    - @Bean ModelMapper
    - @Bean PasswordEncoder
    
    2. SecurityConfig
    - @Bean TokenService
    - @Bean AuthenticationMangeer
    - configure()
    // AuthenticationManagerBuilder, WebSecurity, HttpSecurity

--- src/main/java/me/hjeong/demoinflearnrestapi/DemoApplication.java
+++ src/main/java/me/hjeong/demoinflearnrestapi/DemoApplication.java
@@ -1,9 +1,7 @@
 package me.hjeong.demoinflearnrestapi;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -12,8 +10,4 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
     }
 
-    @Bean
-    public ModelMapper modelWrapper() {
-        return new ModelMapper();
-    }
 }
---
+++ src/main/java/me/hjeong/demoinflearnrestapi/configs/AppConfig.java
@@ -0,0 +1,21 @@
+package me.hjeong.demoinflearnrestapi.configs;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public ModelMapper modelWrapper() {
+        return new ModelMapper();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+}
