a949b718655fcf32aefc0e631928250639dbe983
효정 <hjeong.you@gmail.com>
2020-05-21 20:59:39 +0900

    Runner.run() 에서 Account 저장하기

--- src/main/java/me/hjeong/demoinflearnrestapi/configs/AppConfig.java
+++ src/main/java/me/hjeong/demoinflearnrestapi/configs/AppConfig.java
@@ -1,10 +1,18 @@
 package me.hjeong.demoinflearnrestapi.configs;
 
+import me.hjeong.demoinflearnrestapi.accounts.Account;
+import me.hjeong.demoinflearnrestapi.accounts.AccountRole;
+import me.hjeong.demoinflearnrestapi.accounts.AccountService;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Set;
 
 @Configuration
 public class AppConfig {
@@ -17,5 +25,23 @@ public class AppConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public ApplicationRunner applicationRunner() {
+        return new ApplicationRunner() {
+            @Autowired
+            AccountService accountService;
+
+            @Override
+            public void run(ApplicationArguments args) throws Exception {
+                Account hjeong = Account.builder()
+                        .email("hjeong@email.com")
+                        .password("hjeong")
+                        .roles(Set.of(AccountRole.ADMIN, AccountRole.USER))
+                        .build();
+                accountService.saveAccount(hjeong);
+            }
+        };
     }
 }
