cb1a10876a8808caedb832b4fd4a0a38ed1924b4
효정 <hjeong.you@gmail.com>
2020-05-25 21:55:37 +0900

    4 (스프링 vs 스프링부트) MVC 설정: 프로젝트 세팅

---
+++ _4_spring_boot_web_mvc/src/test/java/me/hjeong/_4_spring_boot_web_mvc/ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hjeong._4_spring_boot_web_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
