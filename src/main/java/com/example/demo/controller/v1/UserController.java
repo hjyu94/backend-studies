5bbcfcc5cdf79661adeeb8b668263cd15837bf0b
효정 <hjeong.you@gmail.com>
2020-06-10 18:35:17 +0900

    Swagger API 문서 자동화

--- src/main/java/com/example/demo/controller/v1/UserController.java
+++ src/main/java/com/example/demo/controller/v1/UserController.java
@@ -2,30 +2,34 @@ package com.example.demo.controller.v1;
 
 import com.example.demo.entity.User;
 import com.example.demo.repo.UserJpaRepo;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api(tags = {"1. User"})
 @RequiredArgsConstructor
 @RestController // 결과값을 JSON으로 출력합니다.
 @RequestMapping(value = "/v1")
 public class UserController {
     private final UserJpaRepo userJpaRepo;
 
+    @ApiOperation(value = "회원 조회", notes = "모든 회원을 조회한다")
     @GetMapping(value = "/user")
     public List<User> findAllUser() {
         return userJpaRepo.findAll();
     }
 
+    @ApiOperation(value = "회원 입력", notes = "회원을 입력한다.")
     @PostMapping(value = "/user")
-    public User save() {
+    public User save(@ApiParam(value = "회원아이디", required = true) @RequestParam String uid,
+                     @ApiParam(value = "회원이름", required = true) @RequestParam String name) {
         User user = User.builder()
-                .uid("yumi@naver.com")
-                .name("유미")
+                .uid(uid)
+                .name(name)
                 .build();
         return userJpaRepo.save(user);
     }
@@ -50,4 +54,16 @@ public class UserController {
     @PostMapping(value = “/user”)
     user테이블에 데이터를 1건 입력합니다. userJpaRepo.save(user); 역시 JPA에서 기본으로 제공하는 메소드입니다. user객체를 전달하면 다음과 같이 내부적으로 insert문을 실행시켜 줍니다.
     insert into user (msrl, name, uid) values (null, ?, ?)
+ */
+/*
+    # Swagger
+    
+    @Api(tags = {“1. User”})
+    UserController를 대표하는 최상단 타이틀 영역에 표시될 값을 세팅합니다.
+
+    @ApiOperation(value = “회원 조회”, notes = “모든 회원을 조회한다”)
+    각각의 resource에 제목과 설명을 표시하기 위해 세팅합니다.
+
+    @ApiParam(value = “회원 이름”, required = true) @RequestParam ~~~
+    파라미터에 대한 설명을 보여주기 위해 세팅합니다.
  */
