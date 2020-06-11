bf1a12e774875e5fd19221008a435d253b409f8c
효정 <hjeong.you@gmail.com>
2020-06-11 21:58:59 +0900

    [h2 연동] 프로젝트 세팅

---
+++ security-database-h2/src/main/java/com/example/securitydatabaseh2/SecurityDatabaseH2Application.java
@@ -0,0 +1,13 @@
+package com.example.securitydatabaseh2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityDatabaseH2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityDatabaseH2Application.class, args);
+    }
+
+}
