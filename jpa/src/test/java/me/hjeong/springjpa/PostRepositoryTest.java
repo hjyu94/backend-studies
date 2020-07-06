01d6291212c1d6370ffff2e42a91a126bd7fa212
효정 <hjeong.you@gmail.com>
2020-07-06 16:02:35 +0900

    [커스텀 리포지토리] 쿼리 확인하기

--- jpa/src/test/java/me/hjeong/springjpa/PostRepositoryTest.java
+++ jpa/src/test/java/me/hjeong/springjpa/PostRepositoryTest.java
@@ -1,9 +1,13 @@
 package me.hjeong.springjpa;
 
+import me.hjeong.springjpa.post.Post;
 import me.hjeong.springjpa.post.PostRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -17,7 +21,29 @@ class PostRepositoryTest {
         // Repository 가 빈으로 잘 등록이 되었나?
         // 등록이 잘 되지 않았다면 테스트가 실패할 것이다.
 
-        postRepository.findMyPost();
+        // 1. 아무 쿼리도 안 날아간다.
+        Post post = new Post();
+        post.setTitle("hibernate");
+        postRepository.save(post);
+        postRepository.delete(post);
     }
 
+    @Test
+    public void crud_2() {
+        Post post = new Post();
+        post.setTitle("hibernate");
+        postRepository.save(post); // insert 쿼리 날아감
+        postRepository.findMyPost(); // select 쿼리 날아감
+        postRepository.delete(post); // 쿼리 날아가지 않음 (어차피 테스트 코드는 롤백 트랜잭션이라서)
+    }
+
+    @Test
+    public void crud_3() {
+        Post post = new Post();
+        post.setTitle("hibernate");
+        postRepository.save(post);
+        postRepository.findMyPost();
+        postRepository.delete(post); // flush() 가 호출되므로 delete 쿼리 날아간다
+        postRepository.flush();
+    }
 }
