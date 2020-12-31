ec36d2e083f1843d58b2797920ce4201c785ae57
Hyojeong Yu <hjeong@lunit.io>
2020-12-31 21:08:31 +0900

    확장모델 (@RunWith(Runner), TestRule, MethodRule -> Extension) : @RegisterExtension

--- _1_junit5/src/test/java/me/hjeong/_1_junit5/FindSlowTestExtensionTest.java
+++ _1_junit5/src/test/java/me/hjeong/_1_junit5/FindSlowTestExtensionTest.java
@@ -2,9 +2,14 @@ package me.hjeong._1_junit5;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.RegisterExtension;
 
-@ExtendWith(FindSlowTestExtension.class)
+//@ExtendWith(FindSlowTestExtension.class)
 public class FindSlowTestExtensionTest {
+
+    @RegisterExtension
+    static FindSlowTestExtension findSlowTestExtension =
+            new FindSlowTestExtension(1000L);
 
     @Test
     void test1() throws InterruptedException {
