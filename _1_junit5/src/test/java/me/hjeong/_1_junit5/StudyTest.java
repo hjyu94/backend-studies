d44c048056977b063add777353d56d76a471ead5
Hyojeong Yu <hjeong@lunit.io>
2020-12-31 18:10:03 +0900

    Test, BeforeAll, AfterAll, BeforeEach, AfterEach, Disabled annotation

---
+++ _1_junit5/src/test/java/me/hjeong/_1_junit5/StudyTest.java
@@ -0,0 +1,47 @@
+package me.hjeong._1_junit5;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class StudyTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("@BeforeAll");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("@AfterAll");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("@BeforeEach");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("@AfterEach");
+    }
+
+    @Test
+    void create() {
+        Study study = new Study();
+        assertNotNull(study);
+        System.out.println("create");
+    }
+
+    @Test
+    void create1() {
+        System.out.println("create1");
+    }
+
+    @Test
+    @Disabled
+    void create2() {
+        System.out.println("create2");
+    }
+
+}
