807286066fe8773d5bbc773bfc4a73c46524874d
Hyojeong Yu <hjeong@lunit.io>
2020-12-31 20:35:13 +0900

    테스트 순서 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)

--- _1_junit5/src/test/java/me/hjeong/_1_junit5/BookTest.java
+++ _1_junit5/src/test/java/me/hjeong/_1_junit5/BookTest.java
@@ -4,11 +4,13 @@ import org.junit.jupiter.api.*;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 //@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class BookTest {
 
     int value = 0;
 
     @Test
+    @Order(1)
     void test_instance_1() {
         value++;
         System.out.println("value = " + value);
@@ -16,6 +18,7 @@ class BookTest {
     }
 
     @RepeatedTest(10)
+    @Order(2)
     void test_instance_2() {
         value++;
         System.out.println("value = " + value);
