113ae4bcfa7b715fdfcf3957b46e5c92a1791829
Hyojeong-Yu <hjeong@lunit.io>
2021-06-05 22:41:37 +0900

    Fix typo

--- src/main/java/me/hjeong/aws_springboot/web/HelloController.java
+++ src/main/java/me/hjeong/aws_springboot/web/HelloController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    //
     @GetMapping("/hello")
     public String hello() {
         return "hello";
