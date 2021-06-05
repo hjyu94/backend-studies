96f2991fe170badd74b7295fb32e0f0b9ad34de8
Hyojeong-Yu <hjeong@lunit.io>
2021-06-05 23:13:26 +0900

    Add: HelloController

--- src/main/java/me/hjeong/aws_springboot/web/HelloController.java
+++ src/main/java/me/hjeong/aws_springboot/web/HelloController.java
@@ -1,6 +1,8 @@
 package me.hjeong.aws_springboot.web;
 
+import me.hjeong.aws_springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 // JSON을 반환하는 컨트롤러
@@ -12,5 +14,15 @@ public class HelloController {
     public String hello() {
         return "hello";
     }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto hello(@RequestParam("name") String name/* 1 */, @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
+    }
+
+    /*
+        1) @RequestParam
+        - 외부에서 API로 넘긴 파라미터를 가져오는 어노테이션
+     */
 
 }
