fe0f00524135ea449828de795ccfcf86b0e55fe9
Hyojeong-Yu <hjeong@lunit.io>
2021-06-18 15:59:27 +0900

    Add: login, logout button and status in /

--- src/main/java/me/hjeong/aws_springboot/web/IndexController.java
+++ src/main/java/me/hjeong/aws_springboot/web/IndexController.java
@@ -1,6 +1,7 @@
 package me.hjeong.aws_springboot.web;
 
 import lombok.RequiredArgsConstructor;
+import me.hjeong.aws_springboot.config.auth.dto.SessionUser;
 import me.hjeong.aws_springboot.service.posts.PostsService;
 import me.hjeong.aws_springboot.web.dto.PostsResponseDto;
 import org.springframework.stereotype.Controller;
@@ -8,16 +9,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         // Model: 서버 템플릿 엔진에서 사용할 수 있는 객체 저장
         model.addAttribute("posts", postsService.findAllDesc());
+        // CustomOAuth2UserService 에서 로그인 성공 시 세션에 저장했던 SessionUser 를 꺼내온다
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
