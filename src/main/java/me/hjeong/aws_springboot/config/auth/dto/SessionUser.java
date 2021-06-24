4dde6507a8b095de147a0f202a5339e3678d24cc
Hyojeong-Yu <hjeong@lunit.io>
2021-06-24 16:29:19 +0900

    Bug fix: serialization session user

--- src/main/java/me/hjeong/aws_springboot/config/auth/dto/SessionUser.java
+++ src/main/java/me/hjeong/aws_springboot/config/auth/dto/SessionUser.java
@@ -3,8 +3,10 @@ package me.hjeong.aws_springboot.config.auth.dto;
 import lombok.Getter;
 import me.hjeong.aws_springboot.domain.user.User;
 
+import java.io.Serializable;
+
 @Getter
-public class SessionUser {
+public class SessionUser implements Serializable {
 
     // SessionUser 에는 인증된 사용자 정보만 필요하다. 그 외에 필요한 정보들은 없으니 name, email, picture만 필드로 선언합니다.
 
