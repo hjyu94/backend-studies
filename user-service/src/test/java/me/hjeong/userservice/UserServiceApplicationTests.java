4cc8fc53ad0476afd60150fa0873ae6dd1f7ad7f
Hyojeong Yu <hjeong.you@gmail.com>
2021-07-24 23:47:42 +0900

    [User Service] 프로젝트 생성

---
+++ user-service/src/test/java/me/hjeong/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hjeong.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
