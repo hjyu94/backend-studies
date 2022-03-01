81ffdea7aa6dbe8b3b578203fec8abbbab13a56e
Hyojeong-Yu <hjeong@lunit.io>
2022-03-01 15:42:01 +0900

    Update: rename board repository to boards repository

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { BoardsController } from './boards.controller';
 import { BoardsService } from './boards.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { BoardRepository } from './board.repository';
+import { BoardsRepository } from './boards.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([BoardRepository])],
+  imports: [TypeOrmModule.forFeature([BoardsRepository])],
   controllers: [BoardsController],
   providers: [BoardsService],
 })
--- src/boards/boards.repository.ts
+++ src/boards/boards.repository.ts
@@ -2,4 +2,4 @@ import { EntityRepository, Repository } from 'typeorm';
 import { Board } from './board.entity';
 
 @EntityRepository(Board)
-export class BoardRepository extends Repository<Board> {}
+export class BoardsRepository extends Repository<Board> {}
