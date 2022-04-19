ff5adc3b6080a2e8e038b21d012632a77808d3ce
Hyojeong-Yu <hjeong@lunit.io>
2022-04-19 09:46:32 +0900

    Add: findById with JDBC

--- src/test/java/hello/jdbc/repository/MemberRepositoryVOTest.java
+++ src/test/java/hello/jdbc/repository/MemberRepositoryVOTest.java
@@ -1,20 +1,28 @@
 package hello.jdbc.repository;
 
 import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+@Slf4j
 class MemberRepositoryVOTest {
 
     MemberRepositoryVO repository = new MemberRepositoryVO();
 
     @Test
     void crud() throws SQLException {
-        Member member = new Member("memberV0", 10000);
+        // save
+        Member member = new Member("memberV1", 10000);
         repository.save(member);
-    }
 
+        // find by id
+        Member findMember = repository.findById(member.getMemberId());
+        log.info("findMember = {}", findMember);
+        log.info("member == findMember : ", member == findMember);
+        log.info("member equals findMember : ", member.equals(findMember));
+        Assertions.assertThat(findMember).isEqualTo(member);
+    }
 }
